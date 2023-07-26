b00b53b31f715588c72d9393aa681558c4b8a145
Anatoliy Shestopalov <shestopalov92@gmail.com>
2023-07-26 21:52:24 +0300

    fix: links render

--- src/Service/LinkRender.php
+++ src/Service/LinkRender.php
@@ -22,24 +22,18 @@ class LinkRender
   public function getQuery()
   {
     $params = $this->request->query;
-    // var_dump($params);
     foreach ($params as $key => $value) {
       if ($key === 'filter') {
         $link = "&" . $key . "=" . $value;
-        // var_dump($link);
-        print $link;
+        echo $link;
       }
     }
-    // if($this->request->query->has('filter')) {
-    //   var_dump($this->request->query->keys());
-    //   var_dump($this->request->query->get('filter'));
-    // }
   }
 
   public function getBasePath(string $path = null, string $var = null): void
   {
     $link = $this->request->getBasePath() . $path . $var;
-    print $link;
+    echo $link;
   }
 
 }
