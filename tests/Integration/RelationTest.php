6b2bcd0cb066585c22b1272396d9782dd31a8835
Anatoliy Shestopalov <shestopalov92@gmail.com>
2023-09-21 23:30:30 +0300

    refactor: edited integration test for table relationship service

--- tests/Integration/RelationTest.php
+++ tests/Integration/RelationTest.php
@@ -3,10 +3,6 @@ declare(strict_types=1);
 
 namespace Tests\Integration;
 
-use App\Repository\ImageRepository;
-use App\Service\OneToManyRelation;
-use App\Models\Image;
-use App\Service\PHPAdventBoardDatabase;
 use App\Service\Relation;
 use PHPUnit\Framework\TestCase;
 
@@ -16,63 +12,52 @@ class RelationTest extends TestCase
     public static function toTestGetRepositoryDataProvider(): array
     {
         return
-        [
-//            'database' => ['databaseName' => 'App\Repository\PHPAdventBoardDatabase'],
-            'repository' => ['repository' => 'App\Repository\ImageRepository'],
-        ];
-    }
-
-
-    public function testGetDatabaseConnection(): PHPAdventBoardDatabase
-    {
-        $databaseConnection = PHPAdventBoardDatabase::getInstance();
-        $this->assertInstanceOf(PHPAdventBoardDatabase::class, $databaseConnection);
-        return $databaseConnection;
+            [
+                'ImageModel' => [
+                    'databaseName' => 'App\Service\PHPAdventBoardDatabase',
+                    'repositoryName' => 'App\Repository\ImageRepository',
+                    'modelName' => 'App\Models\Image',
+                    'imageId' => 27,
+                    'relationColumn' => 'itemId',
+                    'relationType' => 'App\Service\OneToManyRelation'
+                ],
+                'Advert' => [
+                    'databaseName' => 'App\Service\PHPAdventBoardDatabase',
+                    'repositoryName' => 'App\Repository\AdvertRepository',
+                    'modelName' => 'App\Models\Advert',
+                    'advertId' => 1,
+                    'relationColumn' => 'id',
+                    'relationType' => 'App\Service\ManyToOneRelation'
+                ],
+            ];
     }
 
     /**
-     * @depends testGetDatabaseConnection
      * @dataProvider toTestGetRepositoryDataProvider
      */
-    public function testGetRepository($repository, $databaseConnection): ImageRepository
+    public function testGetRelation($databaseName, $repositoryName, $modelName, $imageId, $relationColumn, $relationType)
     {
-        $repository = new $repository($databaseConnection);
-        $this->assertInstanceOf(ImageRepository::class, $repository);
-        return $repository;
-    }
+        $databaseConnection = $databaseName::getInstance();
+        $this->assertInstanceOf($databaseName, $databaseConnection);
 
-    /** @depends testGetRepository */
-    public function testGetModelFromRepository($repository): object
-    {
-        [$model] = $repository->findById(27);
-        $this->assertInstanceOf(Image::class, $model);
-        return $model;
-    }
+        $repository = new $repositoryName($databaseConnection);
+        $this->assertInstanceOf($repositoryName, $repository);
 
-    /** @depends testGetModelFromRepository */
-    public function testRelationInit($model): Relation
-    {
+        [$model] = $repository->findById($imageId);
+        $this->assertInstanceOf($modelName, $model);
+
         $relation = new Relation($model);
         $this->assertInstanceOf(Relation::class, $relation);
-        return $relation;
-    }
 
-    /**
-     * @depends testRelationInit
-     */
-    public function testGetRelationReturnRelationTypeObject($relation): void
-    {
-        $result = $relation->getRelation('itemId');
-
+        $result = $relation->getRelation($relationColumn);
         $model = (array)$result;
         foreach ($model as $property) {
-            // @TODO условие не на проверку объекта, а инстанс от того же типа, который был указан в пустой моделе
-            if (is_object($property)) {
-                $this->assertInstanceOf(OneToManyRelation::class, $property);
-                $this->assertObjectHasProperty('relationModels', $property);
+            if ($property instanceof $relationType) {
+                $this->assertObjectHasProperty('relationModels', $property, "AssertFail: Object $relationType has no property 'relationModels'");
                 $this->assertNotNull($property->relationModels, "AssertFail: Property model has a NULL VALUE");
             }
         }
+//        dd($result);
     }
 
 }
