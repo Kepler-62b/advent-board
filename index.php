36b1ef6a04435df41673721f359c99da00c0469a
Anatoliy Shestopalov <shestopalov92@gmail.com>
2023-09-30 22:47:06 +0300

    style: edited index file

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App;
 
 use App\Service\Helpers\LinkManager;
-use Dev\Service\TestStaticClass;
 use Dev\Tests\TestServices;
 use App\Service\DependencyContainer;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,17 +14,6 @@ require 'vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-$request = Request::createFromGlobals();
-$link = new LinkManager($request);
-$container = new DependencyContainer();
 $test = new TestServices();
 
-// $test->testApp($request);
-
-$user = "root";
-$pass = "";
-$dsn = "mysql:host=localhost;port=3306;dbname=php_advent_board;charset=utf8";
-
-$pdo = new \PDO($dsn, $user, $pass);
-dd($pdo);
-// phpinfo();
+$test->testApp();
