8ba3cccd74847e14cc1c1e04b4888158c5eefc1d
Anatoliy Shestopalov <Alena@MacBook-Pro-Akopova.local>
2023-10-11 16:41:30 +0300

    refactor: moved application entry point

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Framework\Kernel;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+return new Kernel();
