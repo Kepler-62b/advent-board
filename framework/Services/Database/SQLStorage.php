67f20953c93e39cb09910a9010c2436ae9267ae9
Anatoliy Shestopalov <shestopalov92@gmail.com>
2023-10-18 23:36:07 +0300

    feat: implemented sql storage service

--- framework/Services/Database/SQLStorage.php
+++ framework/Services/Database/SQLStorage.php
@@ -7,96 +7,45 @@ use Framework\Services\HydratorService;
 use Framework\Services\NoDBConnectionException;
 use Framework\Services\Relation;
 
-class SQLStorage
+class SQLStorage implements StorageInterface
 {
-    private $connection;
+
+    use SQLQueryTrait;
+
+    private \PDO $pdo;
+
     private $table = 'adverts';
+    private const SELECT_LIMIT = 10;
 
-    public function __construct(PDO $connection)
+    public function __construct(\PDO $pdo)
     {
-        $this->connection = $connection;
+        $this->pdo = $pdo;
     }
 
-    private function pdoStatementHandler()
+    private function pdoStatementHandler(string $sql)
     {
+        $pdoStmt = $this->pdo->prepare($sql);
+        // @TODO логика связывания параметров запроса
 
+        $pdoStmt->execute();
+        $data = $pdoStmt->fetch(\PDO::FETCH_ASSOC);
+        return $data;
     }
 
-    private function statementStorage()
+    public function selectById(int $id): array
     {
+        $sql = $this->select('*', $this->table)
+            ->where('id', $id, '=')
+            ->build();
 
-    }
-
-    private function selectQueryBuilder(string $orderByFilter = null, string $sortDirection = null, int $limit = null, int $offset = null)
-    {
-
-        $sqlSelect = "SELECT * FROM $this->table";
-
-        $sqlQueryParams = [];
-
-        $keywordValue = [
-            '{ORDER BY}' => $orderByFilter,
-            '{sortDirection}' => $sortDirection,
-            '{LIMIT}' => $limit,
-            '{OFFSET}' => $offset,
-        ];
-
-        $keywordMap = [
-            '{ORDER BY}' => 'ORDER BY ',
-            '{LIMIT}' => 'LIMIT ',
-            '{OFFSET}' => 'OFFSET :',
-        ];
-
-        $arrayFilter = array_filter($keywordValue, function ($keyword) {
-            return $keyword;
-        });
-
-
-        foreach ($arrayFilter as $key => $item) {
-            $sqlQueryParams[] = $keywordMap[$key] . $item;
-        }
-
-
-        return $sqlSelect . ' ' . implode(' ', $sqlQueryParams);
-
-    }
-
-    public function selectById(int $id, string $entityClass): ?object
-    {
-        $connection = $this->connection;
-        $table = $this->table;
-
-        $sql = "SELECT * FROM $table WHERE id = :id";
-
-        $pdo_statement = $connection->prepare($sql);
+        var_dump($sql);
 
         try {
-            $pdo_statement->bindValue("id", $id, \PDO::PARAM_INT);
-            $pdo_statement->execute();
-
-            if ($data = $pdo_statement->fetch(\PDO::FETCH_ASSOC)) {
-                // @TODO выбрасывать fatch === false
-//                throw new \Exception("row with id {$id} not found in '{$this->table}' table");
-
-                $hydrator = new HydratorService();
-
-                $model = $hydrator->hydrate(
-                    $entityClass,
-                    $data,
-                    [
-                        'id' => 'id',
-                        'item' => 'item',
-                        'description' => 'description',
-                        'price' => 'price',
-                        'image' => 'image',
-                        'created_date' => 'createdDate',
-                        'modified_date' => 'modifiedDate',
-                    ]
-                );
-                return $model;
-            } else {
-                return NULL;
-            }
+            $pdoStmt = $this->pdo->prepare($sql);
+            $pdoStmt->bindValue(1, $id, \PDO::PARAM_INT);
+            $pdoStmt->execute();
+            $result = $pdoStmt->fetch(\PDO::FETCH_ASSOC);
+            return $result;
         } catch (\PDOException $exception) {
             throw new \PDOException('Ошибка: ' . $exception->getMessage());
         }
@@ -104,54 +53,65 @@ class SQLStorage
 
     public function selectAll(): ?array
     {
-        $connection = $this->pdo;
-        $table = $this->table;
-
-        $sql = "SELECT * FROM $table";
+        $sql = $this->select('*', $this->table);
 
         try {
-            $pdo_statement = $connection->prepare($sql);
-            $pdo_statement->execute();
-            $result = $pdo_statement->fetchAll(\PDO::FETCH_ASSOC);
-
-            $hydrator = new HydratorService();
-            $modelsStorage = [];
-            foreach ($result as $data) {
-                $model = $hydrator->hydrate(
-                    Advert::class,
-                    $data,
-                    [
-                        'id' => 'id',
-                        'item' => 'item',
-                        'description' => 'description',
-                        'price' => 'price',
-                        'image' => 'image',
-                        'created_date' => 'createdDate',
-                        'modified_date' => 'modifiedDate',
-                    ]
-                );
-                $relationModel = (new Relation($model))->getRelation('id');
-                $modelsStorage[] = $relationModel;
-            }
-            return $modelsStorage;
-
+            $pdoStmt = $this->pdo->prepare($sql)->execute();
+            return $pdoStmt->fetchAll(\PDO::FETCH_ASSOC);
         } catch (\PDOException $exception) {
 //            throw new \PDOException($exception);
             throw new NoDBConnectionException("No database connection / $exception");
         }
     }
 
-    public function selectAllWithLimitAndOffset(int $limit, int $dynamicOffset): ?array
+    public function selectBy(array $criteria = null, string $orderBy = null, int $limit = null, int $offset = null): ?array
     {
-        $connection = $this->pdo;
-        $table = $this->table;
-
-        $offset = ($dynamicOffset - 1) * $limit;
-
-        $sql = "SELECT * FROM $table LIMIT $limit OFFSET :offset";
+        $sql = $this->select('*', $this->table)
+            ->where($criteria)
+            ->orderBy($orderBy)
+            ->limit($limit)
+            ->offset($offset)
+            ->build();
 
         try {
-            $pdo_statement = $connection->prepare($sql);
+            if ($pdoStmt = $this->pdo->prepare($sql)->execute()) {
+                return $pdoStmt->fetchAll(\PDO::FETCH_ASSOC);
+            } else {
+                return null;
+            }
+        } catch (\PDOException $exception) {
+//            throw new \PDOException($exception);
+            throw new NoDBConnectionException("No database connection / $exception");
+        }
+    }
+
+    public function selectByOne(array $criteria): ?array
+    {
+        $sql = $this->select('*', $this->table)
+            ->where($criteria)
+            ->build();
+
+        try {
+            $pdoStmt = $this->pdo->prepare($sql)->execute();
+            return $pdoStmt->fetchAll(\PDO::FETCH_ASSOC);
+        } catch (\PDOException $exception) {
+//            throw new \PDOException($exception);
+            throw new NoDBConnectionException("No database connection / $exception");
+        }
+
+    }
+
+    public function selectAllWithOffset(int $offset): ?array
+    {
+        $offset = ($offset - 1) * self::SELECT_LIMIT;
+
+        $sql = $this->select('*', $this->table)
+            ->limit(self::SELECT_LIMIT)
+            ->offset($offset)
+            ->build();
+
+        try {
+            $pdo_statement = $this->pdo->prepare($sql);
             $pdo_statement->bindValue(":offset", $offset, \PDO::PARAM_INT);
             $pdo_statement->execute();
             $result = $pdo_statement->fetchAll(\PDO::FETCH_ASSOC);
@@ -183,19 +143,19 @@ class SQLStorage
         }
     }
 
-    public function selectAllAndSortMax(int $limit, int $dynamicOffset, $sortDirection, $filter): ?array
+    public function selectAllWithOffSortByMax(string $orderBy, int $offset): ?array
     {
-        $connection = $this->pdo;
-        $table = $this->table;
+        $offset = ($offset - 1) * self::SELECT_LIMIT;
 
-
-        $offset = ($dynamicOffset - 1) * $limit;
-
-        $sql = "SELECT * FROM $table ORDER BY $filter DESC LIMIT $limit OFFSET :offset";
-
+        $sql = $this->select('*', $this->table)
+            ->orderBy($orderBy)
+            ->sortDirection('DESC')
+            ->limit(self::SELECT_LIMIT)
+            ->offset($offset)
+            ->build();
 
         try {
-            $pdo_statement = $connection->prepare($sql);
+            $pdo_statement = $this->pdo->prepare($sql);
             $pdo_statement->bindValue(":offset", $offset, \PDO::PARAM_INT);
             $pdo_statement->execute();
 
@@ -222,8 +182,46 @@ class SQLStorage
         } catch (\PDOException $exception) {
             throw new \PDOException($exception);
         }
-
     }
 
+    public function selectAllWithOffSortByMin(string $orderBy, int $offset): ?array
+    {
+        $offset = ($offset - 1) * self::SELECT_LIMIT;
 
+        $sql = $this->select('*', $this->table)
+            ->orderBy($orderBy)
+            ->sortDirection('ASC')
+            ->limit(self::SELECT_LIMIT)
+            ->offset($offset)
+            ->build();
+
+        try {
+            $pdo_statement = $this->pdo->prepare($sql);
+            $pdo_statement->bindValue(":offset", $offset, \PDO::PARAM_INT);
+            $pdo_statement->execute();
+
+            $result = $pdo_statement->fetchAll(\PDO::FETCH_ASSOC);
+
+            $hydrator = new HydratorService();
+            $modelsStorage = [];
+            foreach ($result as $data) {
+                $modelsStorage[] = $hydrator->hydrate(
+                    Advert::class,
+                    $data,
+                    [
+                        'id' => 'id',
+                        'item' => 'item',
+                        'description' => 'description',
+                        'price' => 'price',
+                        'image' => 'image',
+                        'created_date' => 'createdDate',
+                        'modified_date' => 'modifiedDate',
+                    ]
+                );
+            }
+            return $modelsStorage;
+        } catch (\PDOException $exception) {
+            throw new \PDOException($exception);
+        }
+    }
 }
