9fa1f9cc7c8c00414f9555f4032be0d500d8cc31
Anatoliy Shestopalov <shestopalov92@gmail.com>
2023-10-30 22:38:25 +0300

    refactor: adde added a separate file with configurations of connections to the database

--- framework/Services/Database/DatabaseConfigs.php
+++ framework/Services/Database/DatabaseConfigs.php
@@ -4,35 +4,10 @@ namespace Framework\Services\Database;
 
 class DatabaseConfigs
 {
-
-    private function getConfig(): array
-    {
-        return [
-            'PostgreSQL' => [
-                'Driver' => 'pgsql',
-                'Host' => 'postgres',
-                'Port' => '5432',
-                'Database' => 'adverts-board',
-                'User' => 'postgres',
-                'Password' => 'secret',
-            ],
-            'MySQL' => [
-                'Driver' => 'mysql',
-                'Host' => 'mysql',
-                'Database' => 'adverts-board',
-                'User' => 'root',
-                'Password' => 'secret',
-            ],
-            'Redis' => [
-                'Host' => 'redis',
-                'Port' => '6379',
-            ]
-        ];
-    }
-
     public function setConfig(string $driver): array
     {
-        $configMap = $this->getConfig();
+        $configMap = include 'config/databases.php';
+
         $mapParams = $configMap[$driver];
 
         $configs = [];
