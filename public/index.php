4ee8bb5dab8f36107b95ca6b8fd7ab079ecc1b28
Anatoliy Shestopalov <shestopalov92@gmail.com>
2023-10-30 22:32:46 +0300

    style: edited some files

--- public/index.php
+++ public/index.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace App;
 
 use Framework\Kernel;
-use Framework\Services\Database\RedisQueryBuilder;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-// chdir('/app');
 
 return new Kernel(include __DIR__ . '/../config/services.php');
